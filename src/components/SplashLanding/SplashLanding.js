20e07991cccbbe30cbbc6bb3d40d9ca2dbd737f6
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-04-02 14:07:27 -0500

    splash landing html

--- src/components/SplashLanding/SplashLanding.js
+++ src/components/SplashLanding/SplashLanding.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 
+import Login from '../Login/Login';
+
 class SplashLanding extends Component {
     render() {
         return(
-            <div> Landing Page </div>
+            <div>
+                <div className="first-container">
+                    <div>
+                        <h1> Casa Connection </h1> 
+                        <Login />
+                    </div> 
+                    <div>
+                        <h2> Rental Management Simplified </h2>
+                        <button> Get started </button>
+                    </div>
+                </div>
+                <div className="second-container">
+
+                </div>
+                <div className="third-container">
+
+                </div>
+            </div>
         )
     }
 }
