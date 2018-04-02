e2e05d11c6a5a9a2494a932a6f8daa12b3db54f9
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-04-02 14:17:20 -0500

    Image uploader for PDF

--- src/components/Images/PdfUploader.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-
-class PdfUploader extends Component {
-    
-}
--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import PdfUploader from './../ImageUploader/PdfUploader';
 
 class Properties extends Component {
+
     render() {
         return(
             <div> Properties Page </div>
+            
         )
     }
 }
