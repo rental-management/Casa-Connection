76c27ab4e98562baa493d3cb7ca561bbfea7bf12
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-04-03 13:58:13 -0500

    rendering properties

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react';
 import PdfUploader from './../ImageUploader/PdfUploader';
+import {connect} from 'react-redux';
+import {getProperties} from './../../ducks/propertiesReducer';
+
 
 class Properties extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.getProperties();
+    }
 
     render() {
-
+        let propertiesList;
+        console.log(this.props);
+        if(this.props.properties.properties.length !== undefined && this.props.properties.properties.length !== 0) {
+            propertiesList = this.props.properties.properties.map((curr, index) => {
+                return(
+                    <div key={index}>
+                     <h1> {curr.prop_name} </h1>
+                    </div>
+                )
+            })
+        }
         return(
-            <div>
-            <div> Properties Page </div>
-            <PdfUploader />
-            </div>
-          
+            <div> {propertiesList} </div>
         )
     }
 }
-export default Properties;
+const mapStateToProps = state => {
+    return {
+        properties: state.propertiesReducer
+    }
+};
+export default connect(mapStateToProps, {getProperties})(Properties);
---
+++ src/ducks/propertiesReducer.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+// CONSTANTS //
+const GET_PROPERTIES = 'GET_PROPERTIES';
+
+// STATE //
+const initialState = {
+    properties: [],
+    isLoading: false,
+    didErr: false,
+    errMessage: null
+};
+
+// ACTION CREATORS //
+export function getProperties() {
+    return {
+        type: GET_PROPERTIES,
+        payload: axios
+            .get('/properties')
+            .then(response => {
+                console.log(response, "get properties")
+                return response.data
+            }).catch(console.log)
+    }
+}
+
+// REDUCER //
+export default function reducer(state = initialState, action) {
+    console.log(action.type)
+    switch(action.type) {
+        case `${GET_PROPERTIES}_PENDING`:
+            return Object.assign({}, state, { isLoading: true });
+
+        case `${GET_PROPERTIES}_FULFILLED`:
+        console.log(action.payload)
+            return Object.assign({}, state, { isLoading: false, properties: action.payload });
+
+        case `${GET_PROPERTIES}_REJECTED`:
+            return Object.assign({}, state, { isLoading: false, didErr: true, errMessage: action.payload });
+
+    default:
+        return state;
+    }
+}
+
