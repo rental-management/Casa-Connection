fb360750005b6421baa2bffbfed8aca041d0dc74
Michael <Mkoerber7@gmail.com>
2018-04-04 14:40:35 -0500

    Added single property page

--- src/components/SingleProperty/SingleProperty.js
+++ src/components/SingleProperty/SingleProperty.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {getProperty} from '../../ducks/propertiesReducer';
 
 class SingleProperty extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.getProperty(this.props.match.params.id);
+    }
+
     render() {
+        let property;
+        console.log(this.props, "single property render")
+        if(this.props.properties.property !== undefined && this.props.properties.property.length !==0) {
+            property = this.props.properties.property.map((curr, index) => {
+                return (
+                    <div key = {index}>
+                        <h1>{curr.prop_name}</h1>
+                    </div>
+                )
+            })
+        }
         return(
-            <div> Single Property Page </div>
+            <div> Single Property Page
+            {property}
+            </div>
+            
         )
     }
 }
-export default SingleProperty;
+
+
+const mapStateToProps = state => {
+    return {
+        properties: state.propertiesReducer
+    }
+};
+
+export default connect(mapStateToProps, {getProperty})(SingleProperty);
--- src/ducks/propertiesReducer.js
+++ src/ducks/propertiesReducer.js
@@ -3,14 +3,15 @@ import axios from 'axios';
 // CONSTANTS //
 const GET_PROPERTIES = 'GET_PROPERTIES';
 const ADD_PROPERTY = 'ADD_PROPERTY';
+const GET_PROPERTY = "GET_PROPERTY";
 
 // STATE //
 const initialState = {
     properties: [],
+    property: [],
     isLoading: false,
     didErr: false,
     errMessage: null,
-    
 };
 
 // ACTION CREATORS //
@@ -42,6 +43,20 @@ export function addProperty(propertyName, street, city, state, zip){
     }
 }
 
+export function getProperty(id) {
+    return {
+        type: GET_PROPERTY,
+        payload: axios
+        .get(`/property/${id}`,{
+            id: id
+        })
+        .then(response => {
+            console.log(response, "getProperty")
+            return response.data
+        }).catch(console.log)
+    }
+}
+
 // REDUCER //
 export default function reducer(state = initialState, action) {
     console.log(action.type)
@@ -58,7 +73,7 @@ export default function reducer(state = initialState, action) {
             return Object.assign({}, state, { isLoading: false, didErr: true, errMessage: action.payload });
 
             //ADD PROPERTY
-             case `${ADD_PROPERTY}_PENDING`:
+        case `${ADD_PROPERTY}_PENDING`:
             return Object.assign({}, state, {isLoading: true});
 
         case `${ADD_PROPERTY}_FULFILLED`:
@@ -66,6 +81,17 @@ export default function reducer(state = initialState, action) {
 
         case `${ADD_PROPERTY}_REJECTED`:
             return Object.assign({}, state, {isLoading: false, didErr: true, errMessage: action.payload});
+
+        // GET SINGLE PROPERTY
+        case `${GET_PROPERTY}_PENDING`:
+            return Object.assign({}, state, { isLoading: true });
+
+        case `${GET_PROPERTY}_FULFILLED`:
+        console.log(action.payload, "get prop full")
+            return Object.assign({}, state, { isLoading: false, property: action.payload });
+
+        case `${GET_PROPERTY}_REJECTED`:
+            return Object.assign({}, state, { isLoading: false, didErr: true, errMessage: action.payload });
          
 
     default:
