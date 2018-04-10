45165f0f7437c2b7b4bd1560ad66c8ccb6beea35
Michael <Mkoerber7@gmail.com>
2018-04-10 14:41:01 -0500

    finished delete work order

--- db/getAllWorkOrders.sql
+++ db/getAllWorkOrders.sql
@@ -1,2 +1,3 @@
-SELECT * FROM work_orders JOIN properties ON work_orders.prop_id = properties.id
+SELECT * FROM work_orders JOIN properties ON
+work_orders.prop_id = properties.id 
 WHERE work_orders.user_id = $1;
